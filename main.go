999c4ef60dcfdf5f1951098654c71d6bee3dd300
onwl007 <onwl007@126.com>
2020-09-06 19:10:04 +0800

    map

--- main.go
+++ main.go
@@ -4,72 +4,59 @@ import "fmt"
 
 func main() {
 	/**
-	切片是对数组的抽象，数组的长度是不可变的，切片本身没有任何数据，只是对现有数组的引用
-	切片像一个结构体，包含三个元素
-	1.指针，指向数组中 slice 指定的开始位置
-	2.长度，即 slice 的长度
-	3.最大长度，也就是 slice 开始位置到数组的最后位置的长度
-
-	当多个切片共享相同的底层数组时，每个元素所做的更改将在数组中反映出来
-
-	语法
-		var identifier []type
-	使用 make() 函数创建切片
-		var slice1 []type = make([]type, len)
-		slice1 := make([]type, len)
+	Map 无序的键值对集合，引用类型
+	注意
+		1：map 是无序的，不能通过 index 获取，必须通过 key 获取
+		2：长度不固定，引用类型
+		3：内置的 len 函数同样适用于 map，返回 map 拥有的 key 的数量
+		4：map 的 key 可以是所有可比较的类型，如布尔型、整数型、浮点型、复杂型、字符串型
 	*/
-	a := [5]int{76, 77, 78, 79, 80}
-	var b []int = a[1:4] // 前闭后开
-	fmt.Println(b)
+	var countryCapitalMap map[string]string
+	countryCapitalMap = make(map[string]string)
 
-	darr := [...]int{57, 89, 90, 82, 100, 78, 67, 69, 59}
-	dslice := darr[2:5] // 90, 82, 100
-	fmt.Println("array before", darr)
-	for i := range dslice {
-		dslice[i]++
+	// map 插入 key-value 对，各个多家对应的首都
+	countryCapitalMap["France"] = "Paris"
+	countryCapitalMap["Italy"] = "Rome"
+	countryCapitalMap["Japan"] = "Tokyo"
+	countryCapitalMap["India"] = "New Delhi"
+
+	for country := range countryCapitalMap {
+		fmt.Println("Capital of", country, "is", countryCapitalMap[country])
 	}
-	fmt.Println("array after", darr)
 
-	// 多个切片共享相同的数组时，每个切片改变元素的值，数组中都会改变
-	numa := [3]int{78, 79, 80}
-	nums1 := numa[:] // 创建一个切片包含所有元素
-	nums2 := numa[:]
-	fmt.Println("array befire change 1 ", numa)
-	nums1[0] = 100
-	fmt.Println("array after modification to slice nums1", numa)
-	nums2[1] = 101
-	fmt.Println("array after modification to slice nums2", numa)
+	// 查看元素在集合中是否存在
+	captial, ok := countryCapitalMap["United States"]
+	if ok {
+		fmt.Println("Capital of United States is ", captial)
+	} else {
+		fmt.Println("Capital of United States is not present")
+	}
 
-	/**
-	切片
-		长度：切片中元素的数量
-		容量：从创建切片的索引开始的底层数组中元素的数量
-	*/
-	var slice2 = make([]int, 3, 5)
-	printSlice(slice2)
+	delete(countryCapitalMap, "France")
+	fmt.Println("Entry for France is deleted")
 
-	var numbers []int
-	printSlice(numbers)
+	fmt.Println("删除元素后 map")
 
-	// 允许追加空切片
-	numbers = append(numbers, 0)
-	printSlice(numbers)
+	for country := range countryCapitalMap {
+		fmt.Println("Capital of ", country, "is ", countryCapitalMap[country])
+	}
 
-	numbers = append(numbers, 1)
-	printSlice(numbers)
+	m := make(map[string]int)
+	m["a"] = 1
+	x, ok := m["b"]
+	fmt.Println(x, ok)
+	x, ok = m["a"]
+	fmt.Println(x, ok)
 
-	numbers = append(numbers, 2, 3, 4)
-	printSlice(numbers)
-
-	// 创建切片 numbers1 是之前切片的两倍容量
-	numbers1 := make([]int, len(numbers), (cap(numbers))*2)
-
-	// 拷贝 numbers 的内容到 numbers1
-	copy(numbers1, numbers)
-	printSlice(numbers1)
-
-}
-
-func printSlice(x []int) {
-	fmt.Printf("len = %d cap = %d slice = %v\n", len(x), cap(x), x)
+	// map 是引用类型，将映射分配给一个新变量时，它们都指向相同的内部数据结构，因此，一个的变化会反映另一个
+	// map 不能使用 == 操作符进行比较。== 只能用来检查 map 是否为空。否则会报错
+	personSalary := map[string]int{
+		"steve": 12000,
+		"jamie": 15000,
+	}
+	personSalary["mike"] = 9000
+	fmt.Println("Original person salary", personSalary)
+	newPersonSalary := personSalary
+	newPersonSalary["mike"] = 18000
+	fmt.Println("Person salary changed", personSalary)
 }
