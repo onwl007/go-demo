0b6234fd382601802ff0518f8aa0832fe7cf1fe5
onwl007 <onwl007@126.com>
2020-09-07 23:04:16 +0800

    method

--- main.go
+++ main.go
@@ -1,96 +1,56 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
-type name int8
-type first struct {
-	a int
-	b bool
-	name
+type Employee struct {
+	name     string
+	salary   int
+	currency string
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+type Circle struct {
+	radius float64
+}
+
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func (c Circle) area() float64 {
+	return c.radius * c.radius * math.Pi
+}
+
+func (e Employee) displaySalary() {
+	fmt.Printf("Slary of %s is %s%d\n", e.name, e.currency, e.salary)
 }
 
 func main() {
 	/**
-	变量
-		是一种使用方便的占位符，用于引用计算机内存地址
-	指针
-		是存储另一个变量内存地址的变量
-	声明指针
-		var var_name *var_type
-		var ip *int 指向整型
-		var fp *float32 指向浮点型
-	& 取址符
-	获取指针的值，语法：*a
-
-	不要将一个指向数组的指针传递给函数，使用切片
-
-	指针的指针
-		一个指针变量存放的又是另一个指针变量的地址，则称这个指针变量为指向指针的指针变量
-		var ptr **int
+	方法：
+		就是一个包含了接受者的函数，接受者可以是命名类型或者结构体类型的一个值或是指针
+	语法
+		func (t Type) methodName(parma list)(return list) {}
 	*/
-	var a int = 20
-	var ip *int
+	emp1 := Employee{
+		name:     "Sam Adolf",
+		salary:   5000,
+		currency: "$",
+	}
+	emp1.displaySalary()
 
-	ip = &a // 指针变量的存储地址
-	fmt.Printf("a 变量的地址是：%x\n", &a)
-
-	// 指针变量的存储地址
-	fmt.Printf("ip 变量的存储地址：%x\n", ip)
-
-	// 使用指针访问值
-	fmt.Printf("*ip 变量的值：%d\n", *ip)
-
-	var s = first{1, false, 2}
-	var b *first = &s
-	fmt.Println(s.b, s.a, s.name, &s, b.a, &b, (*b).a)
-
-	m := 255
-	n := &m
-	fmt.Println("address of m is ", n)
-	fmt.Println("value of m is ", *n)
-	*n++
-	fmt.Println("new value of m is ", m)
-
-	x := 58
-	fmt.Println("value of x before function call is ", x)
-	y := &x
-	change(y)
-	fmt.Println("value of x after function call is ", x)
-
-	var c int
-	var ptr *int
-	var pptr **int
-	c = 3000
-	// 指针 ptr 地址
-	ptr = &c
-	// 指向指针 ptr 地址
-	pptr = &ptr
-	// 获取 pptr 的值
-	fmt.Printf("变量 c = %d\n", c)
-	fmt.Printf("指针变量 *ptr = %d\n", *ptr)
-	fmt.Printf("指向指针的指针变量 **ptr = %d\n", **pptr)
-
-	var e int = 100
-	var f int = 200
-	fmt.Printf("交换前 e 的值 : %d\n", e)
-	fmt.Printf("交换前 f 的值 : %d\n", f)
-
-	/* 调用函数用于交换值
-	 * &a 指向 a 变量的地址
-	 * &b 指向 b 变量的地址
-	 */
-	swap(&e, &f)
-	fmt.Printf("交换后 e 的值 : %d\n", e)
-	fmt.Printf("交换后 f 的值 : %d\n", f)
-}
-
-func change(val *int) {
-	*val = 55
-}
-
-func swap(x, y *int) {
-	var temp int
-	temp = *x
-	*x = *y
-	*y = temp
+	r1 := Rectangle{12, 2}
+	r2 := Rectangle{9, 4}
+	c1 := Circle{10}
+	c2 := Circle{25}
+	fmt.Println("Area of r1 is: ", r1.area())
+	fmt.Println("Area of r2 is: ", r2.area())
+	fmt.Println("Area of c1 is: ", c1.area())
+	fmt.Println("Area of c2 is: ", c2.area())
 }
