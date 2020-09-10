dbdd7c93ebf228a5d3216e08e53e8e2415db1cc2
onwl007 <onwl007@126.com>
2020-09-10 23:19:52 +0800

    error1

--- main.go
+++ main.go
@@ -2,133 +2,64 @@ package main
 
 import (
 	"fmt"
-	"go-demo/employee"
+	"math"
 )
 
-type author struct {
-	firstName string
-	lastName  string
-	bio       string
+type areaError struct {
+	err    string
+	radius float64
 }
 
-func (a author) fullName() string {
-	return fmt.Sprintf("%s %s", a.firstName, a.lastName)
-}
-
-type post struct {
-	title   string
-	content string
-	author
-}
-
-func (p post) details() {
-	fmt.Println("Title: ", p.title)
-	fmt.Println("Content: ", p.content)
-	fmt.Println("Author: ", p.author.fullName())
-	fmt.Println("Bio: ", p.author.bio)
-}
-
-type website struct {
-	posts []post
-}
-
-func (w website) contents() {
-	fmt.Println("Contents of Website")
-	for _, v := range w.posts {
-		v.details()
-		fmt.Println()
-	}
-}
-
-type Income interface {
-	calculate() int
-	source() string
-}
-
-type FixedBilling struct {
-	projectName  string
-	biddedAmount int
-}
-
-type TimeAndMaterial struct {
-	projectName string
-	noOfHours   int
-	hourlyRate  int
-}
-
-func (fb FixedBilling) calculate() int {
-	return fb.biddedAmount
-}
-
-func (fb FixedBilling) source() string {
-	return fb.projectName
-}
-
-func (tm TimeAndMaterial) calculate() int {
-	return tm.noOfHours * tm.hourlyRate
-}
-
-func (tm TimeAndMaterial) source() string {
-	return tm.projectName
-}
-
-type Advertisement struct {
-	adName     string
-	CPC        int
-	noOfClicks int
-}
-
-func (a Advertisement) calculate() int {
-	return a.CPC * a.noOfClicks
-}
-
-func (a Advertisement) source() string {
-	return a.adName
-}
-
-func calculateNetIncome(ic []Income) {
-	var netincome int = 0
-	for _, income := range ic {
-		fmt.Printf("Income From %s = %d\n", income.source(), income.calculate())
-		netincome += income.calculate()
-	}
-	fmt.Printf("Net income of organisation = $%d", netincome)
+func (e *areaError) Error() string {
+	return fmt.Sprintf("radius %0.2f: %s", e.radius, e.err)
 }
 
 func main() {
-	e := employee.New("Sam", "Adolf", 30, 20)
-	e.LeavesRemaining()
+	// f, err := os.Open("/test.txt")
+	// if err, ok := err.(*os.PathError); ok {
+	// 	fmt.Println("File at path", err.Path, "failed to open")
+	// 	return
+	// }
+	// fmt.Println(f.Name(), "opened successfully")
 
-	author1 := author{
-		"Naveen",
-		"Ramanathan",
-		"Golang Enthusiast",
-	}
-	post1 := post{
-		"Inheritance in Go",
-		"Go supports composition instead of inheritance",
-		author1,
-	}
-	post2 := post{
-		"Struct instead of Classes in Go",
-		"Go does not support classes but methods can be added to structs",
-		author1,
-	}
-	post3 := post{
-		"Concurrency",
-		"Go is a concurrent language and not a parallel one",
-		author1,
-	}
-	w := website{
-		posts: []post{post1, post2, post3},
-	}
-	w.contents()
+	// addr, err := net.LookupHost("golangbot123.com")
+	// if err, ok := err.(*net.DNSError); ok {
+	// 	if err.Timeout() {
+	// 		fmt.Println("operation timed out")
+	// 	} else if err.Temporary() {
+	// 		fmt.Println("temporary error")
+	// 	} else {
+	// 		fmt.Println("generic error", err)
+	// 	}
+	// 	return
+	// }
+	// fmt.Println(addr)
 
-	project1 := FixedBilling{projectName: "Project 1", biddedAmount: 5000}
-	project2 := FixedBilling{projectName: "Project 2", biddedAmount: 10000}
-	project3 := TimeAndMaterial{projectName: "Project 3", noOfHours: 160, hourlyRate: 25}
-	bannerAd := Advertisement{adName: "Banner Ad", CPC: 2, noOfClicks: 500}
-	popupAd := Advertisement{adName: "Popup Ad", CPC: 5, noOfClicks: 750}
-	incomeStreams := []Income{project1, project2, project3, bannerAd, popupAd}
-	calculateNetIncome(incomeStreams)
+	// files, error := filepath.Glob("[")
+	// if error != nil && error == filepath.ErrBadPattern {
+	// 	fmt.Println(error)
+	// 	return
+	// }
+	// fmt.Println("matched files", files)
+
+	// 自定义错误
+	// 可以使用 errors 包下的 New() 函数，以及 fmt 包下的 Errorf() 函数
+	radius := -20.0
+	area, err := circleArea(radius)
+	if err != nil {
+		if err, ok := err.(*areaError); ok {
+			fmt.Printf("Radius %0.2f is less than zero", err.radius)
+			return
+		}
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Area of circle %0.2f", area)
+}
+
+func circleArea(radius float64) (float64, error) {
+	if radius < 0 {
+		return 0, &areaError{"radius is negative", radius}
+	}
+	return math.Pi * radius * radius, nil
 }
