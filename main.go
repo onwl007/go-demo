7f93361f97904f9fd9a848baefb204a5c1effba1
onwl007 <onwl007@126.com>
2020-09-10 23:29:35 +0800

    custom error

--- main.go
+++ main.go
@@ -1,65 +1,57 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+import "fmt"
 
 type areaError struct {
 	err    string
-	radius float64
+	length float64
+	width  float64
 }
 
 func (e *areaError) Error() string {
-	return fmt.Sprintf("radius %0.2f: %s", e.radius, e.err)
+	return e.err
+}
+
+func (e *areaError) lengthNegative() bool {
+	return e.length < 0
+}
+
+func (e *areaError) widthNegative() bool {
+	return e.width < 0
+}
+
+func rectArea(length, width float64) (float64, error) {
+	err := ""
+	if length < 0 {
+		err += "length is less than zero"
+	}
+	if width < 0 {
+		if err == "" {
+			err = "width is less than zero"
+		} else {
+			err += ", width is less than zero"
+		}
+	}
+	if err != "" {
+		return 0, &areaError{err, length, width}
+	}
+	return length * width, nil
 }
 
 func main() {
-	// f, err := os.Open("/test.txt")
-	// if err, ok := err.(*os.PathError); ok {
-	// 	fmt.Println("File at path", err.Path, "failed to open")
-	// 	return
-	// }
-	// fmt.Println(f.Name(), "opened successfully")
-
-	// addr, err := net.LookupHost("golangbot123.com")
-	// if err, ok := err.(*net.DNSError); ok {
-	// 	if err.Timeout() {
-	// 		fmt.Println("operation timed out")
-	// 	} else if err.Temporary() {
-	// 		fmt.Println("temporary error")
-	// 	} else {
-	// 		fmt.Println("generic error", err)
-	// 	}
-	// 	return
-	// }
-	// fmt.Println(addr)
-
-	// files, error := filepath.Glob("[")
-	// if error != nil && error == filepath.ErrBadPattern {
-	// 	fmt.Println(error)
-	// 	return
-	// }
-	// fmt.Println("matched files", files)
-
-	// 自定义错误
-	// 可以使用 errors 包下的 New() 函数，以及 fmt 包下的 Errorf() 函数
-	radius := -20.0
-	area, err := circleArea(radius)
+	length, width := -5.0, -9.0
+	area, err := rectArea(length, width)
 	if err != nil {
 		if err, ok := err.(*areaError); ok {
-			fmt.Printf("Radius %0.2f is less than zero", err.radius)
-			return
+			if err.lengthNegative() {
+				fmt.Printf("error: kength %0.2f is less than zero\n", err.length)
+			}
+			if err.widthNegative() {
+				fmt.Printf("error: width %0.2f is less than zero\n", err.width)
+			}
 		}
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("Area of circle %0.2f", area)
-}
-
-func circleArea(radius float64) (float64, error) {
-	if radius < 0 {
-		return 0, &areaError{"radius is negative", radius}
-	}
-	return math.Pi * radius * radius, nil
+	fmt.Println("area of rect", area)
 }
