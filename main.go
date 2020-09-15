6d3612e4785259865aa745c95ce0cef33d67fccb
onwl007 <onwl007@126.com>
2020-09-15 22:26:19 +0800

    reflect call

--- main.go
+++ main.go
@@ -19,6 +19,10 @@ func (p Person) PrintInfo() {
 	fmt.Printf("姓名：%s，年龄：%d，性别：%s\n", p.Name, p.Age, p.Sex)
 }
 
+func (p Person) Test(i, j int, s string) {
+	fmt.Println(i, j, s)
+}
+
 func main() {
 	/**
 	反射
@@ -45,37 +49,33 @@ func main() {
 		6. 从 Value 的指针到值
 		7. Type 指针和值的相互转换
 		8. Value 值的可修改性
+
+	如何通过反射来进行方法的调用？
+	本来可以用结构体对象，方法名称直接调用的
+	但如果要通过反射，首先要将方法注册，也就是 MethodByName，然后通过反射调用 Call
 	*/
 
-	p1 := Person{"王二狗", 30, "男"}
-	DoFiledAndMethod(p1)
+	p2 := Person{"Ruby", 30, "男"}
+	// 1. 要通过反射来调用起对应的方法，必须要先通过 reflect.ValueOf(interface)来获取到 reflect.Value,得到反射类型对象后才能做下一步处理
+	getValue := reflect.ValueOf(p2)
 
-}
+	// 2. 一定要指定参数为正确的方法名，先看看没有参数的调用方法
+	methodValue1 := getValue.MethodByName("PrintInfo")
+	fmt.Printf("Kind: %s, Type: %s\n", methodValue1.Kind(), methodValue1.Type())
+	methodValue1.Call(nil) // 没有参数，直接写 nil
 
-// 通过接口来获取任意参数
-func DoFiledAndMethod(input interface{}) {
-	getType := reflect.TypeOf(input)
-	fmt.Println("get Type is: ", getType.Name())
-	fmt.Println("get Kind is: ", getType.Kind())
+	args1 := make([]reflect.Value, 0) // 或者创建一个空的切片也可以
+	methodValue1.Call(args1)
 
-	getValue := reflect.ValueOf(input)
-	fmt.Println("get all Fields is: ", getValue)
+	// 有参数的方法调用
+	methodValue2 := getValue.MethodByName("Say")
+	fmt.Printf("Kind: %s, Type: %s\n", methodValue2.Kind(), methodValue2.Type())
+	args2 := []reflect.Value{reflect.ValueOf("反射机制")}
+	methodValue2.Call(args2)
 
-	// 获取方法字段
-	// 1. 先获取 interface 的 reflect.Type，然后通过 NumField 进行遍历
-	// 2. 再通过 reflect.Type 的 Field 获取其 Field
-	// 3. 最后通过 Field 的 Interface() 得到对应的 value
-	for i := 0; i < getType.NumField(); i++ {
-		field := getType.Field(i)
-		value := getValue.Field(i).Interface()
-		fmt.Printf("字段名称：%s，字段类型：%s，字段数值：%v \n", field.Name, field.Type, value)
-	}
+	methodValue3 := getValue.MethodByName("Test")
+	fmt.Printf("Kind: %s, Type: %s\n", methodValue3.Kind(), methodValue3.Type())
+	args3 := []reflect.Value{reflect.ValueOf(100), reflect.ValueOf(200), reflect.ValueOf("Hello")}
+	methodValue3.Call(args3)
 
-	// 通过反射，操作方法
-	// 1. 先获取 interface 的 reflect.Type，然后通过 NumMethod 进行遍历
-	// 2. 再通过 reflect.Type 的 Method 获取其 Method
-	for i := 0; i < getType.NumMethod(); i++ {
-		method := getType.Method(i)
-		fmt.Printf("方法名称：%s，方法类型：%v \n", method.Name, method.Type)
-	}
 }
