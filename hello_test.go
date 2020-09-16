8fc3cd558db47b8dc886ff743e3dcba9a343c0c9
onwl007 <onwl007@126.com>
2020-09-16 23:13:17 +0800

    hellor world unit test

--- hello_test.go
+++ hello_test.go
@@ -8,10 +8,24 @@ import "testing"
 3. 测试函数只接受一个参数 t *testing.T
 */
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
 
-	if got != want {
-		t.Errorf("got '%q' want '%q'", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// 告诉测试套件这个方法时辅助函数，当测试失败时所报告的行号酱紫啊函数调用中而不是在辅助函数内部
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
 }
