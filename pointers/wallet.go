68d74b0d11c45dbe9aabadc94a5aea0dba9c58ce
onwl007 <onwl007@126.com>
2020-09-21 21:47:24 +0800

    pointers test

--- pointers/wallet.go
+++ pointers/wallet.go
@@ -2,17 +2,31 @@ package pointers
 
 import "fmt"
 
+type Bitcoin int
+
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
 type Wallet struct {
-	balance int
+	balance Bitcoin
 }
 
 // 在 Go 中，当调用一个函数或方法时，参数会被复制
 // 当调用这个方法时，w 来自我们调用这个方法的副本
-func (w *Wallet) Deposit(amount int) {
+func (w *Wallet) Deposit(amount Bitcoin) {
 	fmt.Println("address of balance in Deposit is ", &w.balance)
 	w.balance += amount
 }
 
-func (w *Wallet) Balance() int {
+func (w *Wallet) Balance() Bitcoin {
 	return w.balance
+}
+
+func (w *Wallet) Widthdraw(amount Bitcoin) {
+	w.balance -= amount
 }
--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -6,18 +6,35 @@ import (
 )
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
 
-	wallet.Deposit(10)
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
 
-	got := wallet.Balance()
+		wallet.Deposit(Bitcoin(10))
 
-	// 当我们在代码中更改 balance 的值时，我们处理的是来自测试的副本，因此，balance 在测试中没有被改变
-	fmt.Println("address of balance in test is ", &wallet.balance)
+		got := wallet.Balance()
 
-	want := 10
+		// 当我们在代码中更改 balance 的值时，我们处理的是来自测试的副本，因此，balance 在测试中没有被改变
+		fmt.Println("address of balance in test is ", &wallet.balance)
 
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
-	}
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Widthdraw(10)
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	})
 }
