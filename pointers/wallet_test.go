42b5d5bc364e3256e4aab120da571115db4f93a5
onwl007 <onwl007@126.com>
2020-09-21 21:52:12 +0800

    pointers test

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -1,40 +1,28 @@
 package pointers
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestWallet(t *testing.T) {
 
-	t.Run("Deposit", func(t *testing.T) {
-		wallet := Wallet{}
-
-		wallet.Deposit(Bitcoin(10))
-
+	assertBalance := func(t *testing.T, wallet Wallet, want Bitcoin) {
 		got := wallet.Balance()
-
-		// 当我们在代码中更改 balance 的值时，我们处理的是来自测试的副本，因此，balance 在测试中没有被改变
-		fmt.Println("address of balance in test is ", &wallet.balance)
-
-		want := Bitcoin(10)
 
 		if got != want {
 			t.Errorf("got %s want %s", got, want)
 		}
+	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(20)}
-
-		wallet.Widthdraw(10)
-
-		got := wallet.Balance()
-
-		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		wallet.Widthdraw(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
 	})
 }
