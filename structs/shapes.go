be7afe3b010fe3d918988471f109139e41d89c2f
onwl007 <onwl007@126.com>
2020-09-21 21:12:41 +0800

    struct test

--- structs/shapes.go
+++ structs/shapes.go
@@ -2,6 +2,10 @@ package structs
 
 import "math"
 
+type Shape interface {
+	Area() float64
+}
+
 type Rectangle struct {
 	Width  float64
 	Height float64
@@ -19,6 +23,11 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
-type Shape interface {
-	Area() float64
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func (c Triangle) Area() float64 {
+	return (c.Base * c.Height) * 0.5
 }
