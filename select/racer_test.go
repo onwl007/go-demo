d670ab1495c85505cb04fea37c904840987c38c2
onwl007 <onwl007@126.com>
2020-09-24 21:49:27 +0800

    select test

---
+++ select/racer_test.go
@@ -0,0 +1,36 @@
+package racer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestRacer(t *testing.T) {
+	slowServer := makeDelayedServer(20 * time.Millisecond)
+	fastServer := makeDelayedServer(0 * time.Millisecond)
+
+	defer slowServer.Close()
+	defer fastServer.Close()
+
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got '%s', want '%s'", got, want)
+	}
+
+	slowServer.Close()
+	fastServer.Close()
+}
+
+func makeDelayedServer(delay time.Duration) *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		time.Sleep(delay)
+		rw.WriteHeader(http.StatusOK)
+	}))
+}
