cdf5f75ca05902be3780b3827e6c86ff5b67d727
onwl007 <onwl007@126.com>
2020-09-24 21:59:40 +0800

    select test

--- select/racer.go
+++ select/racer.go
@@ -1,13 +1,19 @@
 package racer
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
-func Racer(a, b string) (winner string) {
+func Racer(a, b string, timeout time.Duration) (winner string, error error) {
 	select {
 	case <-ping(a):
-		return a
+		return a, nil
 	case <-ping(b):
-		return b
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
 }
 
--- select/racer_test.go
+++ select/racer_test.go
@@ -8,24 +8,21 @@ import (
 )
 
 func TestRacer(t *testing.T) {
-	slowServer := makeDelayedServer(20 * time.Millisecond)
-	fastServer := makeDelayedServer(0 * time.Millisecond)
 
-	defer slowServer.Close()
-	defer fastServer.Close()
+	t.Run("returns an error if a server doesn't respond within 10s", func(t *testing.T) {
+		serverA := makeDelayedServer(11 * time.Second)
+		serverB := makeDelayedServer(12 * time.Second)
 
-	slowURL := slowServer.URL
-	fastURL := fastServer.URL
+		defer serverA.Close()
+		defer serverB.Close()
 
-	want := fastURL
-	got := Racer(slowURL, fastURL)
+		_, err := Racer(serverA.URL, serverB.URL)
 
-	if got != want {
-		t.Errorf("got '%s', want '%s'", got, want)
-	}
+		if err == nil {
+			t.Error("expected an error but didn't get one")
+		}
 
-	slowServer.Close()
-	fastServer.Close()
+	})
 }
 
 func makeDelayedServer(delay time.Duration) *httptest.Server {
